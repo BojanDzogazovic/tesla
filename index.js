f49a4d15e10d5507a8c1760f7d441745dfa469ca
Bojan Dzogazovic <bdzogazovic@gmail.com>
2019-03-18 17:40:31 +0100

    Updates

---
+++ index.js
@@ -0,0 +1,66 @@
+  window.onscroll = function(){
+    let topBtn = document.getElementById("gototop");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        topBtn.style.opacity = "1";
+    } else {
+        topBtn.style.opacity = "0";
+    }
+
+}; 
+ 
+window.onload = function(){
+   let videoBg = document.getElementById("myVideo");
+  videoBg.play();
+}
+
+//slick slider
+$('.fade').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    arrows:true,
+    centerMode: true,
+    centerPadding: "50px",
+    mobileFirst: true,
+    cssEase: 'linear'
+  });
+
+
+document.getElementById("mobiledrop").addEventListener("click", function(){
+    let navbar = document.getElementById("navbar");
+    if (navbar.className === "navbar") {
+      navbar.className += " responsive";
+    } else {
+      navbar.className = "navbar";
+    }
+});
+
+//smooth scrool
+//smooth scroll
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
